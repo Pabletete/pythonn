35c93664a7d5cb743435f32b35d5732a4de0cb69
Pabletete <116726461+Pabletete@users.noreply.github.com>
2023-04-20 12:49:00 +0200

    Delete Excercici.9.py

--- Excercici.9.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-
-print("     Excercici 9""\n")
-a = input("Introdueix el teu Nom: ")
-b = input("Introdueix el teu Cognom: ")
-system('clear')
-print("\n""Bon dia,",a,b,"")
-
